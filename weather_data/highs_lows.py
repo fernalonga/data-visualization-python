aeb9ef931e01a88a2ecc384a7a2a43daacb1f9e1
Fernando Gomes <fernalongar@gmail.com>
2018-11-11 11:33:38 -0200

    Update weather_data project, improve Stika measures

--- weather_data/highs_lows.py
+++ weather_data/highs_lows.py
@@ -3,25 +3,31 @@ from datetime import datetime
 from matplotlib import pyplot as plt
 
 # Get dates and high temperatures from CSV file.
-filename = 'sitka_weather_07-2014.csv'
+filename = 'sitka_weather_2014.csv'
 with open(filename) as f:
     reader = csv.reader(f)
     header_row = next(reader)
     
-    dates, highs = [], []
+    dates, highs, lows = [], [], []
     for row in reader:
+        # Extract dates.
         current_date = datetime.strptime(row[0], "%Y-%m-%d")
         dates.append(current_date)
-
+        # Extract high temperatures.
         high = int(row[1])
         highs.append(high)
+        # Extract low temperatures.
+        low = int(row[3])
+        lows.append(low)
 
 # Plot data.
 fig = plt.figure(dpi=128, figsize=(10, 6))
-plt.plot(dates, highs, c='red')
+plt.plot(dates, highs, c='red', alpha=0.5)
+plt.plot(dates, lows, c='blue', alpha=0.5)
+plt.fill_between(dates, highs, lows, facecolor='blue', alpha=0.1)
 
 # Format plot.
-plt.title("Daily high temperatures, July 2014", fontsize=14)
+plt.title("Daily high and low temperatures - 2014", fontsize=24)
 plt.xlabel('', fontsize=16)
 fig.autofmt_xdate()
 plt.ylabel("Temperature (F)", fontsize=16)
